b8cb59559cd556bfa71b72053c26bfc26221581d
Tamirat <tamworku1.com>
2020-06-03 03:06:43 +0200

    init commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const User = require('../../models/users.js')
 router.use('/add', (req, res) => {
     const newUser = req.body;
     console.log(newUser)
-    const user = User.create( {name:newUser.name, surname:newUser.surname,  email:newUser.email, password: newUser.password  } )
+    const user = User.create( {name:newUser.name, surname:newUser.surname, birthDate:newUser.birthDate,  email:newUser.email, password: newUser.password  } )
     .then( result => console.log(result))
     .catch( err => console.log(err)) 
 })
